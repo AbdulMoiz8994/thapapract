ef18d69e45c7aa0674cff33e403fdb864aeaa865
AbdulMoiz8994 <moiza8994@gmail.com>
2021-01-18 14:26:20 +0500

    add these chnages we made and also we are doing react practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,70 @@
 import React from 'react';
-
+import './App.css'
 
 function App(){
-  const names="abdul moiz"
-  const lastNames="Attari"
-   const currentDate= new Date().toLocaleDateString()
-   const currentTime= new Date().toLocaleTimeString()
+  // const names="abdul moiz"
+  // const lastNames="Attari"
+  //  const currentDate= new Date().toLocaleDateString()
+  //  const currentTime= new Date().toLocaleTimeString()
+  //  const imgs="https://picsum.photos/250/300"
+  //  const imgs1="https://picsum.photos/250/300"
+  //  const imgs2="https://picsum.photos/250/300"
+
+  //  const heading={
+  //     color: '#207398',
+  //     margin: '40px 50px',
+  //     textAlign: 'center',
+  // textTransform: 'capitalize',
+  // fontSize: 'x-large',
+  // textShadow: '2px 3px 2px gray',
+  // fontFamily: "'Open Sans', sans-serif", 
+  //  }
+ let dates= new Date(2021,1,18,14);
+ dates= dates.getHours();
+ let changes='';
+ let colors={};
+ if(dates >=1 && dates <=12){
+   changes="Good Morning"
+   colors.color="green"
+
+ }else if(dates >12 && dates <=19){
+    changes="Good Evening"
+    colors.color="orange"
+ }else{
+   changes="Good Night"
+   colors.color="black"
+
+ }
   return(
     <div>
+      <div>
+
+<h1 style={{color: '#E03B8B'}} className="h1">hello sir, <span style={colors}>{changes}</span></h1>
+
+
+
+      </div>
+
+      {/* <h1 style={heading}>Thapa Technical React Practice before</h1>
+      <h1 style={{color: 'blue', textAlign: 'center', fontFamily: 'sans-serif'}}>Thapa Technical React Practice before</h1>
+
+      <h1 className="heading">Thapa Technical React Practice before</h1>
+      
+      <div className="imgss">
+      <img src={imgs} alt="random images"/>
+      <img src={imgs1} alt="random images"/>
+      <img src={imgs2} alt="random images"/>
+      
+      </div> */}
+      {/* <h2>{`Hello my name is ${names} ${lastNames}`}</h2>
+      <p>Today Date is: {currentDate}</p>
+      <p>The Current Time is:{currentTime}</p> */}
       {/* <h1>{`hello world my name is ${names} and my last name is ${lastNames} ${5+5}`}</h1> */}
       {/* <h1>My name is {names}</h1> */}
       {/* <h2>My lucky number is {Math.random()}</h2> */}
-      <h1>Thapa Technical React Practice</h1>
-      <h2 contentEditable="inherit">{`Hello my name is ${names} ${lastNames}`}</h2>
-      <p>Today Date is: {currentDate}</p>
-      <p>The Current Time is:{currentTime}</p>
+      {/* <img src="https://fontawesome.com/icons/user-nurse?style=solid" alt=""/> */}
+      {/* <div style={{width: "100%"}}><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=karachi+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="map"></iframe> */}
+      {/* <a href="https://www.maps.ie/route-planner.htm">Google Route Planner</a></div> */}
     </div>
   )
 }
