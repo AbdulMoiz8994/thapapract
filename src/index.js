a2e0c984feb6dd8a6117a970a3b994a954eae308
AbdulMoiz8994 <moiza8994@gmail.com>
2021-03-01 19:52:02 +0500

    add these changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
 
-function Hi() {
-  return (
-    <div>
-      <App />
-      {/* <h1> {5 + 5}</h1> */}
-      Hello World!
-    </div>
-  );
-  function Hi(name) {
-    return console.log(name);
-  }
-  Hi();
-}
-// ReactDOM.render(React.createElement(Hi), document.getElementById("root"));
+function Hi({ names, width, options: { fulName, fatherName }, arry }) {
+  console.log(names, width, fulName, fatherName, arry);
 
-ReactDOM.render(<Hi />, document.getElementById("root"));
+  return `Hi ${
+    names === true ? "Open" : "Close"
+  } and the width is ${width} and details are ${fulName}  father name is ${fatherName} and array ${arry}`;
+}
+ReactDOM.render(
+  <Hi
+    names={false}
+    width={30}
+    options={{ fulName: "Abdul moiz", fatherName: "Zafar Ali" }}
+    arry={[1, 2, 4, 5]}
+  />,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
